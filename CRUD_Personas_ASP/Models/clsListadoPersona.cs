2c5ae069d3f3c6ca0398749b023f49fbd677e9c3
Jesús <jesussantostorres@gmail.com>
2021-11-17 23:57:56 +0100

    Meto proyectos

---
+++ CRUD_Personas_ASP/Models/clsListadoPersona.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using DAL;
+using Entities;
+
+namespace Ejercicio2.Models
+{
+    public class clsListadoPersona
+    {
+
+        private ClaseListadoPersonaDAL listaDAL;
+
+        public clsListadoPersona()
+        {
+            listaDAL = new ClaseListadoPersonaDAL();
+        }
+
+        public ObservableCollection<clsPersona> GetListadoClsPersonas() {
+            return listaDAL.getUsuariosCompletos();
+        }
+    }
+}
---
+++ CRUD_Personas_DAL/Listados/ClaseListadoPersonaDAL.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Data.SqlClient;
+using DAL;
+using Entities;
+
+
+
+namespace DAL
+{
+
+    public class ClaseListadoPersonaDAL
+    {
+        private clsMyConnection myConnection;
+        private SqlConnection sqlConnection;
+        private ObservableCollection<clsPersona> listaPersonas;
+
+        public ClaseListadoPersonaDAL()
+        {
+            myConnection = new clsMyConnection();
+            listaPersonas = new ObservableCollection<clsPersona>();
+        }
+
+        public ObservableCollection<clsPersona> getUsuariosCompletos()
+        {
+            clsPersona oPersona;
+            SqlCommand command = new SqlCommand();
+            sqlConnection = myConnection.getConnection();
+            SqlDataReader reader;
+            command.CommandText = "SELECT * FROM Personas";
+            command.Connection = sqlConnection;
+            reader = command.ExecuteReader();
+
+            try 
+            {
+                if (reader.Read())
+                {
+                    while (reader.Read())
+                    {
+                        oPersona = new clsPersona();
+                        oPersona.Id = (int)reader["IDPersona"];
+                        oPersona.Nombre = (string)reader["nombrePersona"];
+                        oPersona.Apellidos = (string)reader["apellidosPersona"];
+                        oPersona.Direccion = (string)reader["direccion"];
+                        oPersona.Telefono = (string)reader["telefono"];
+                        if (reader["fechaNacimiento"] != System.DBNull.Value)
+                        {
+                            oPersona.FechaNacimiento = (DateTime)reader["fechaNacimiento"];
+                        }
+                        listaPersonas.Add(oPersona);
+                    }
+                }
+                reader.Close();
+                sqlConnection.Close();
+            }
+            catch (Exception e) { }
+
+            return listaPersonas;
+        }
+    }
+}
