748a2f5b6d3cf8a4518c7f7c2eade4731390e610
Jesús <jesussantostorres@gmail.com>
2021-11-29 14:36:44 +0100

    Algo de controllers

---
+++ CRUD_Personas_ASP/Models/ViewModels/VM_CreatePersonaListaDepartamentos.cs
@@ -0,0 +1,37 @@
+﻿using BL;
+using CRUD_Personas_BL.Listados;
+using CRUD_Personas_Entities;
+using System.Collections.ObjectModel;
+
+namespace CRUD_Personas_ASP.Models.ViewModels
+{
+    public class VM_CreatePersonaListaDepartamentos
+    {
+        private clsPersona persona;
+        private ObservableCollection<clsDepartamento> departamentos;
+
+        public VM_CreatePersonaListaDepartamentos()
+        {
+            persona = new clsPersona();
+            departamentos = new clsListadoDepartamentosBL().Departamentos;
+        }
+
+        public VM_CreatePersonaListaDepartamentos(int id)
+        {
+            persona = clsListadoPersonasBL(id);
+            departamentos = new clsListadoDepartamentosBL().Departamentos;
+        }
+
+        public clsPersona Persona
+        {
+            get { return persona; }
+            set { persona = value; }
+        }
+
+        public ObservableCollection<clsDepartamento> Departamentos
+        {
+            get { return departamentos; }
+            set { departamentos = value; }
+        }
+    }
+}
