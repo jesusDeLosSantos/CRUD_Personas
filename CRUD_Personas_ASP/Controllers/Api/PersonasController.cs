c1cbde8186ea92748045e10463bc2b599a413433
Jesús <jesussantostorres@gmail.com>
2022-01-13 14:30:48 +0100

    Cosas de API

--- CRUD_Personas_ASP/Controllers/Api/PersonasController.cs
+++ CRUD_Personas_ASP/Controllers/Api/PersonasController.cs
@@ -2,6 +2,7 @@
 using CRUD_Personas_Entities;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using CRUD_Personas_BL.Gestoras;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -9,7 +10,7 @@ namespace CRUD_Personas_ASP.Api
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class Personas : ControllerBase
+    public class PersonasController : ControllerBase
     {
         // GET: api/<Personas>
         [HttpGet]
@@ -20,27 +21,30 @@ namespace CRUD_Personas_ASP.Api
 
         // GET api/<Personas>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public clsPersona Get(int id)
         {
-            return "value";
+            return new clsListadoPersonasBL(id).Persona;
         }
 
         // POST api/<Personas>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post([FromBody] clsPersona persona)
         {
+            GestoraAccionesPersonasBL.addPersonaBL(persona);
         }
 
         // PUT api/<Personas>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public void Put(int id, [FromBody] clsPersona persona)
         {
+            GestoraAccionesPersonasBL.alterPersonaBL(persona);
         }
 
         // DELETE api/<Personas>/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            GestoraAccionesPersonasBL.deletePersonaBL(id);
         }
     }
 }
